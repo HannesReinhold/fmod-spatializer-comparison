abf01df9c2b9ce7c7de3330f3837a12b7af9aabe
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2024-06-21 14:04:14 +0200

    Fix footprints

--- FMOD Spatializer Comparison/Assets/Evaluation App/Scripts/Artistic/Industry/TrailManager.cs
+++ FMOD Spatializer Comparison/Assets/Evaluation App/Scripts/Artistic/Industry/TrailManager.cs
@@ -12,7 +12,7 @@ public class TrailManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        trailObjects[0].SetActive(true);
+        //trailObjects[0].SetActive(true);
     }
 
     // Update is called once per frame
@@ -21,10 +21,49 @@ public class TrailManager : MonoBehaviour
         
     }
 
+    public void ShowPart()
+    {
+        Invoke("ShowPath1", 1);
+        Invoke("ShowPath2", 1.5f);
+        Invoke("ShowPath3", 2f);
+        Invoke("ShowPath4", 2.5f);
+        Invoke("ShowPath5", 3f);
+    }
+
     public void SetNextPart()
     {
         currenttrailObject++;
-        trailObjects[currenttrailObject - 1].SetActive(false);
+        //trailObjects[currenttrailObject - 1].SetActive(false);
         trailObjects[currenttrailObject].SetActive(true);
+        Debug.Log("Show Footstep "+currenttrailObject);
+    }
+
+    private void ShowPath1()
+    {
+        trailObjects[0].SetActive(true);
+        Debug.Log("Show Footstep ");
+    }
+    private void ShowPath2()
+    {
+        trailObjects[1].SetActive(true);
+        Debug.Log("Show Footstep ");
+    }
+
+    private void ShowPath3()
+    {
+        trailObjects[2].SetActive(true);
+        Debug.Log("Show Footstep ");
+    }
+
+    private void ShowPath4()
+    {
+        trailObjects[3].SetActive(true);
+        Debug.Log("Show Footstep ");
+    }
+
+    private void ShowPath5()
+    {
+        trailObjects[4].SetActive(true);
+        Debug.Log("Show Footstep ");
     }
 }
