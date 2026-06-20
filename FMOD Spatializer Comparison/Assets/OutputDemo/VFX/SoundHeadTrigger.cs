a3b28da1f98e13f04df32496ba3e2d39aac5492e
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2026-06-20 14:17:26 +0200

    Add audio propagation and head visualizer

---
+++ FMOD Spatializer Comparison/Assets/OutputDemo/VFX/SoundHeadTrigger.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class SoundHeadTrigger : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    public Collider triggerCollider1;
+    public Collider triggerCollider2;
+    public MeshRenderer meshRendererLeft;
+    public MeshRenderer meshRendererRight;
+
+    // Verbleibende Lebenszeit, sobald ein Partikel den Trigger erreicht
+    public float lifetimeInsideTrigger = 0.2f;
+
+    private ParticleSystem.Particle[] particles;
+
+    private float smoothedHighlightValueLeft = 0;
+    private float smoothedHighlightValueRight = 0;
+
+    void Update()
+    {
+
+        int count = particleSystem.particleCount;
+
+        if (particles == null || particles.Length < count)
+            particles = new ParticleSystem.Particle[count];
+
+        count = particleSystem.GetParticles(particles);
+
+        bool isLocal = particleSystem.main.simulationSpace == ParticleSystemSimulationSpace.Local;
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 particlePos = isLocal
+                ? particleSystem.transform.TransformPoint(particles[i].position)
+                : particles[i].position;
+
+            if (triggerCollider1.bounds.Contains(particlePos))
+            {
+                particles[i].remainingLifetime = Mathf.Min(
+                    particles[i].remainingLifetime,
+                    lifetimeInsideTrigger
+                );
+                smoothedHighlightValueLeft = Mathf.Max(smoothedHighlightValueLeft, particles[i].remainingLifetime);
+                smoothedHighlightValueLeft = Mathf.Clamp(smoothedHighlightValueLeft,0,1);
+            }
+
+            if (triggerCollider2.bounds.Contains(particlePos))
+            {
+                smoothedHighlightValueRight = Mathf.Max(smoothedHighlightValueRight, particles[i].remainingLifetime);
+                smoothedHighlightValueRight = Mathf.Clamp(smoothedHighlightValueRight, 0, 1);
+            }
+
+        }
+        
+
+        meshRendererLeft.material.SetFloat("_Highlighting", smoothedHighlightValueLeft);
+        meshRendererRight.material.SetFloat("_Highlighting", smoothedHighlightValueRight);
+
+        smoothedHighlightValueLeft = Mathf.Lerp(smoothedHighlightValueLeft, 0, Time.deltaTime * 7);
+        smoothedHighlightValueRight = Mathf.Lerp(smoothedHighlightValueRight, 0, Time.deltaTime * 7);
+
+        particleSystem.SetParticles(particles, count);
+    }
+}
